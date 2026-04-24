9d03ff15dfced46c7802f730f12c2501a75fdab0
somya singh <somya.21b0131037@abes.ac.in>
2026-04-24 12:50:20 +0530

    call button changes

--- src/components/FloatngSocialButtons.js
+++ src/components/FloatngSocialButtons.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import { FaWhatsapp, FaPhone } from "react-icons/fa";
 import SocialButton from "./SocialButton";
 
 export default function FloatingSocialButtons() {
@@ -8,18 +8,18 @@ export default function FloatingSocialButtons() {
 
       {/* WhatsApp */}
       <SocialButton
-        href="https://wa.me/+91-8287333505"
+        href="https://wa.me/919319913232"
         title="Chat on WhatsApp"
         colorClass="bg-green-500 hover:bg-green-600"
         icon={<FaWhatsapp size={24} />}
       />
 
-      {/* Instagram */}
+      {/* Call */}
       <SocialButton
-        href="https://www.instagram.com/crowninktattoos/"
-        title="Visit Instagram"
-        colorClass="bg-gradient-to-tr from-pink-500 via-red-500 to-yellow-500"
-        icon={<FaInstagram size={24} />}
+        href="tel:+919319913232"
+        title="Call Now"
+        colorClass="bg-blue-500 hover:bg-blue-600"
+        icon={<FaPhone size={24} />}
       />
 
     </div>
